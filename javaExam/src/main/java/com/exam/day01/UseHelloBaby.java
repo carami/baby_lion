782b3848021b0390d14fa8e191d4504bf7c15732
carami <carami@nate.com>
2024-03-18 20:36:11 +0900

    day01 추가

---
+++ javaExam/src/main/java/com/exam/day01/UseHelloBaby.java
@@ -0,0 +1,9 @@
+package com.exam.day01;
+
+public class UseHelloBaby {
+    public static void main(String[] args) {
+        HelloBaby hb = new HelloBaby(); //인스턴스화
+        hb.say("hello");
+        HelloBaby.print("hi");
+    }
+}
