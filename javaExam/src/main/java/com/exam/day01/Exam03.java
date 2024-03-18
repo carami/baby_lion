a5d52f17ca333c3dbd64e3f3e1517eed6d40c640
carami <carami@nate.com>
2024-03-18 21:59:49 +0900

    update exam

--- javaExam/src/main/java/com/exam/day01/Exam03.java
+++ javaExam/src/main/java/com/exam/day01/Exam03.java
@@ -1,5 +1,7 @@
 package com.exam.day01;
 
+import java.util.Scanner;
+
 public class Exam03 {
     //학생수를 입력하세요.   (입력받은 학생수에 해당하는..)
     //학생 5명의 점수를 입력받아서
@@ -8,5 +10,30 @@ public class Exam03 {
 
 
     //main..
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("학생 수를 입력하세요.");
+        int student = sc.nextInt();
+        //System.out.println(student);
+
+        int[] arr;
+        arr = new int[student];
+
+        for(int i = 0; i < student; i++){
+            System.out.println("점수를 입력하세요.");
+            int score = sc.nextInt();
+            arr[i] = /*sc.nextInt();*/ score;
+            System.out.println("score" + arr[i]);
+        }
+
+        int sum = 0;
+
+        for(int j = 0; j < student; j++){
+            sum += arr[j];
+        }
+//
+        int average = sum / student;
+        System.out.println("sum : " + sum + " " + "average : " + average);
+    }
 
 }
